617e993a7c1ae12028a1b1305339e89a8f66d69d
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-01-20 18:21:04 +0900

    [Bronze III] Title: 팩토리얼 진법, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5692. 팩토리얼 진법/팩토리얼 진법.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+int Factorial(int);
+
+int main() 
+{
+    int quit = 0;
+    while (quit == 0)
+    {
+        int sum = 0;
+        char n[5] = "";
+        scanf("%s", n);
+
+        if (n[0] == '0')
+        {
+            quit = 1;
+            break;
+        }
+
+        for (int i = 0; i < strlen(n); i++)
+        {
+            int temp = Factorial(strlen(n)-i);
+            while (temp != 0)
+            {
+                sum += n[i]-'0';
+                temp--;
+            }
+        }
+        printf("%d\n", sum);
+        
+    }
+}
+
+int Factorial(int i)
+{
+    int result=1;
+    for (i; i > 0; i--)
+    {
+        result *= i;
+    }
+    return result;
+}
