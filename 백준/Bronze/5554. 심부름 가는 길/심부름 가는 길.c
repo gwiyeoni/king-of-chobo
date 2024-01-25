ebbe2096ff2e855b49376e5d284d4f716c2c6dc2
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-01-25 21:05:24 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.c
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.c
@@ -8,26 +8,14 @@
 int main(void)
 {
     int second[4] = { 0 };
-    int min = 0;
-    int sec = 0;
+    int sum=0, min, sec;
+
     for (int i = 0; i < 4; i++) {
         scanf("%d", &second[i]);
-
-        if (second[i] > 60) {
-            min += second[i] / 60;
-            sec += second[i] % 60;
-        }
-        else
-        {
-            sec += second[i];
-        }
-        if (sec >= 60)
-        {
-            min++;
-            sec -= 60;
-        }
-
+        sum += second[i];
     }
-    printf("%d\n", min);
-    printf("%d", sec);
+    min = sum / 60;
+    sec = sum % 60;
+
+    printf("%d\n%d", min, sec);
 }
