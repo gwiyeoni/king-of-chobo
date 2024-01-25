8174fc1e44a41467bb0f8418a1c540eaba6b134e
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-01-26 00:04:20 +0900

    [Bronze II] Title: 팰린드롬, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10174. 팰린드롬/팰린드롬.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+
+
+int main(void)
+{
+    //앞뒤가 똑같은 전화번호 1577!
+    int n;
+    scanf("%d", &n);
+    getchar();
+    char arr[19] = { 0 };
+    for (int i = 0; i < n; i++)
+    {
+        int same = 1;
+
+        scanf("%[^\n]s", arr);
+        getchar();
+        for (int j = 0; j <= strlen(arr)/2; j++)
+        {
+            
+            if (tolower(arr[j]) != tolower(arr[strlen(arr) - j-1]))
+            {
+                printf("No\n");
+                same = 0;
+                break;
+            }
+        }
+        if (same == 1)
+            printf("Yes\n");
+    }
+}
