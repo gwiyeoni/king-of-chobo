906edf133f683899a968c48d76a5f251c07b02bc
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-03-11 01:20:26 +0900

    [Bronze I] Title: 논리학 교수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1813. 논리학 교수/논리학 교수.c
+++ 백준/Bronze/1813. 논리학 교수/논리학 교수.c
@@ -1,26 +1,45 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<math.h>
 
-int main() {
-    int N, i, j, count = 0, maxTruth = -1;
-    scanf("%d", &N); // 항승이가 한 말의 개수 N 입력
-    int statements[N]; // 정수 배열 선언
-    
-    for(i = 0; i < N; i++) {
-        scanf("%d", &statements[i]); // 각 말에 대한 정수 입력
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    int s, button=0;
+    int num[51] = { 0 };
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &s);
+        num[s]++;
     }
-    
-    for(i = 0; i <= N; i++) { // 가능한 참인 말의 개수를 0부터 N까지 검사
-        count = 0;
-        for(j = 0; j < N; j++) {
-            if(statements[j] == i) {
-                count++; // 현재 가정한 참인 말의 개수가 실제로 몇 개인지 세기
+
+    int temp = 0;
+
+    if (num[0] == n)
+    {
+        printf("-1");
+    }
+    else
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            if (num[i]==i)
+            {
+                temp = i;
+                button = 1;
             }
         }
-        if(count == i) { // 가정한 참인 말의 개수와 실제로 센 개수가 같다면
-            maxTruth = i; // 최대 참인 말의 개수 업데이트
+
+        if (button==1)
+        {
+            printf("%d", temp);
         }
+        else printf("-1");
+            
     }
 
-    printf("%d\n", maxTruth); // 결과 출력
-    return 0;
 }
