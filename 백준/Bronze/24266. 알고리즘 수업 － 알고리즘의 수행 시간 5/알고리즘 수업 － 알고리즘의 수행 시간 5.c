8de4f31f4ab10c4a3c16db392070c83ce689cd61
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-03-24 22:57:07 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+int main(void)
+{
+	long long int n;
+	scanf("%lld", &n);
+	printf("%lld\n", n*n*n);
+	printf("3");
+}
