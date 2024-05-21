dcdffba08bd9cd2515514cf40a400428a79ca38f
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-05-21 23:04:14 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#define _CRT_NO_SECURE_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double A, B;
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A / B;
+}
