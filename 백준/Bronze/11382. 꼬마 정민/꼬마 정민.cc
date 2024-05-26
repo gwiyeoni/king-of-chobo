84993771515200c1c1896e34391cde5e87630ca1
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-05-26 21:13:10 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#define _CRT_NO_SECURE_WARNINGS
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+}
