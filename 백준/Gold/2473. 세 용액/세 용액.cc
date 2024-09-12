eb2cfded77df4facebc0dc2a2f7bc6f5a1a58c5c
gwiyomi <126251913+gwiyeoni@users.noreply.github.com>
2024-09-12 11:58:56 +0900

    [Gold III] Title: 세 용액, Time: 28 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<ll> solution(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> solution[i];
+    }
+
+    sort(solution.begin(), solution.end());
+
+    ll left = 0, mid, right = N - 1, sum = 0, min_sum = LONG_MAX;
+    tuple<ll,ll,ll> result = { 0,0,0 };
+    while (left < N - 2)
+    {
+        mid = left + 1, right = N - 1;
+        while (mid < right)
+        {
+            sum = solution[left] + solution[mid] + solution[right];
+            if (min_sum > abs(sum))
+            {
+                min_sum = abs(sum);
+                result = { solution[left], solution[mid], solution[right] };
+            }
+
+            if (sum < 0)
+                mid++;
+            else
+                right--;
+
+        }
+        left++;
+    }
+
+    auto [a, b, c] = result;
+
+    cout << a << " " << b << " " << c << "\n";
+}
